d7aae6855e9ae78d4caa2ff4e8722005ec434818
Sebi Francis <sebifrancis1935@gmail.com>
2025-08-02 19:41:43 +0530

    Day 4

--- Ecommerce/Shop/views.py
+++ Ecommerce/Shop/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Sebi Francis's Ecommerce Shop")
+
+def home(request):
+    return HttpResponse("Welcome to the Ecommerce Shop!")
+
+def about(request):
+    return HttpResponse("About the Ecommerce Shop!")
+
+def contact(request):
+    return HttpResponse("This is a contact page!")
