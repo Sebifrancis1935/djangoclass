48f755cb72d1fb8d122d40eebf33a625d25f50f3
Sebifrancis1935 <119755302+Sebifrancis1935@users.noreply.github.com>
2025-08-02 22:54:35 +0530

    Day 5

--- Ecommerce/Shop/views.py
+++ Ecommerce/Shop/views.py
@@ -1,13 +1,14 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Sebi Francis's Ecommerce Shop")
 
 def home(request):
-    return HttpResponse("Welcome to the Ecommerce Shop!")
+    return render(request, 'shop/home.html')
 
 def about(request):
-    return HttpResponse("About the Ecommerce Shop!")
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse("This is a contact page!")
